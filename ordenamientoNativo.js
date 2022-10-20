31eea65285ac8b7279b6619a1ca65d887997d520
Ronaldo Arias <ronaldorafaelap@ufps.edu.co>
2022-10-20 15:38:35 -0500

    metodos de ordenamiento nativo del lenguaje

---
+++ ordenamientoNativo.js
@@ -0,0 +1,12 @@
+
+ const arr = [17,19,24,32,41,57,62];
+ const arr1 = [62,57,41,32,24,19,17];
+ const arr2 = [17,24,41,62,19,32,57];
+
+
+console.log("output arr");
+console.log(arr.sort());
+console.log("output arr1");
+console.log(arr1.sort());
+console.log("output arr2");
+console.log(arr2.sort());
